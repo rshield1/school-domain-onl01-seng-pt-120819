14651c163d285b11144989718ebb69e428c7a095
Robert Shields <robert.shieldsiv@gmail.com>
2020-01-01 19:17:16 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -7,7 +7,7 @@ class School
   end
 
   def add_student(student_name, student_grade)
-    roster[student_grade] ||= []
+    roster[student_grade] = []
     roster[student_grade] << student_name
   end
 
